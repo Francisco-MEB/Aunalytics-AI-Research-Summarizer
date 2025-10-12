0bfe9122b18bd586a0b926f5232e50bf0b13bd8c
Lexie Leija <lexieleija@mac.dhcp.nd.edu>
2025-10-12 14:43:06 -0400

    check

--- store.py
+++ store.py
@@ -1,11 +1,11 @@
 import psycopg2
 
-conn= psycopg2.connect("your_supabase_connection_string")
+conn= psycopg2.connect("postgresql://postgres:[DATACSE2025]@db.vrnkkwhqymobivddukqc.supabase.co:5432/postgres")
 cur= conn.cursor()
 
 cur.execute("""
     CREATE TABLE IF NOT EXISTS documents(
-        id SEIRAL PRIMARY KEY,
+        id SERIAL PRIMARY KEY,
         content TEXT,git 
         metadata JSONB,
         embedding VECTOR(384)
