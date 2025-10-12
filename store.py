246ff43625d75376a1cbdf28c507b53d35102efc
Lexie Leija <lexieleija@mac.dhcp.nd.edu>
2025-10-12 12:39:19 -0400

    initial commit for storing

---
+++ store.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+conn= psycopg2.connect("your_supabase_connection_string")
+cur= conn.cursor()
+
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS documents(
+        id SEIRAL PRIMARY KEY,
+        content TEXT,
+        metadata JSONB,
+        embedding VECTOR(384)
+);
+""")
+
+conn.commit()
+cur.close()
+conn.close()
+
