35243b704ef886d0284f073f07fdfec2cb0e019b
Peter Bae <peterbae@dhcp-10-24-115-21.dhcp.nd.edu>
2025-10-12 12:03:08 -0400

    initial commit

--- qa_system.py
+++ qa_system.py
@@ -1,1 +1,15 @@
 #made a file for the qa system 
+from langchain_core.documents import Document
+from typing_extensions import List, TypedDict
+from sentence_transformers import SentenceTransformer
+
+
+# keep track of the input question, retrieved context, and generated answer
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+
+
+
