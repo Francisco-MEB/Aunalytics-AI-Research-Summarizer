18cc45912d7955385a95ab6d5d96aee35b7b2002
eavi23 <eaviles@nd.edu>
2025-10-16 19:49:54 -0400

    fixed up prompts.py

--- prompts.py
+++ prompts.py
@@ -1,5 +1,15 @@
 from langchain.prompts import ChatPromptTemplate
 
-SYSTEM_INSTRUCTION = ChatPromptTemplate(
-    ("system", "You are a helpful AI bot")
+
+SYSTEM_INSTRUCTION = (
+    [
+        ("system", "You are a helpful AI with expertise in the research areas and details  of numerous professors. Your job is to simplify and put information into Layman's terms for the user interaction."),
+    ]
+)
+
+template = ChatPromptTemplate.from_messages(
+    {
+        ("system", SYSTEM_INSTRUCTION),
+        ("human", "CONTEXT:\n{context}\n\nQUESTION: {question}"),
+    }
 )
